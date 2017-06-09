f4f4da2d1f4c2ac96d0d09b1a98a747f61d9f020
ecoinclub <ecoinclubs@gmail.com>
2017-06-09 11:40:04 +0530

    Update Protocol Version

--- src/version.h
+++ src/version.h
@@ -30,10 +30,10 @@ static const int DATABASE_VERSION = 70507;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 90000;
+static const int PROTOCOL_VERSION = 90001;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+static const int MIN_PROTO_VERSION = 90001;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
